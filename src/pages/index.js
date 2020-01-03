ba4c7bb155dfba6a60981e106d61d11b4b929f86
nailerHeum <kingman330@gmail.com>
2020-01-03 12:35:56 +0900

    FIX react-helmet on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import Head from "../components/head"
 
 const IndexPage = () => {
   return (
     <div>
       <Layout>
+        <Head title="Home" />
         <h1>Hello</h1>
         <h2>I'm Nailer, the Backend-developer.</h2>
       </Layout>
