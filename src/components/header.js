b455c480944753e1baded2c8922c62b9e4083ffa
nailerHeum <kingman330@gmail.com>
2020-01-09 14:51:20 +0900

    ADD cute imoji

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ const Header = () => {
     <header className={headerStyles.header}>
       <h1>
         <Link className={headerStyles.title} to="/">
-          {data.site.siteMetadata.title}
+          {data.site.siteMetadata.title} + "공사중 👷‍♂️"
         </Link>
       </h1>
       <nav>
