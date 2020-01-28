f0d454482a70646954111cfadae174fafd895c66
nailerHeum <kingman330@gmail.com>
2020-01-28 15:30:19 +0900

    FIX index page to show posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,7 @@
-import React from "react";
-
-import Layout from "../components/layout";
-import Head from "../components/head"
+import PostPage from "./post";
 
 const IndexPage = () => {
-  return (
-    <div>
-      <Layout>
-        <Head title="Home" />
-        <h1>Hello</h1>
-        <h2>I'm Nailer, the Backend-developer.</h2>
-      </Layout>
-    </div>
-  );
+  return PostPage();
 }
 
 export default IndexPage;
